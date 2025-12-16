b5ad0dc4c23dcfb991b9c21a5281e8e92b28e612
Andrew-157 <subotinandrey5@gmail.com>
2025-12-16 20:14:39 +0100

    list only unique interfaces

--- main.c
+++ main.c
@@ -20,6 +20,17 @@ int main() {
     for (struct ifaddrs *ifa = ifaddr; ifa != NULL; ifa = ifa->ifa_next) {
         //family = ifa->ifa_addr->sa_family;
 
+        int iface_listed = 0;
+        for (int i = 0; i < count; i++) {
+            if (strcmp(names[i], ifa->ifa_name) == 0) {
+                iface_listed = 1;
+                break;
+            }
+        }
+        if (iface_listed) {
+            continue;
+        }
+
         // accoring to `man realloc`:
         // If ptr is NULL, then the call is equivalent to malloc(size), for all values of size
         names = (char **)realloc(names, (count + 1) * sizeof(char *));
