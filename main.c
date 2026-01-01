26b8eb481e956aa63a90e57cd06a88ed122733fd
Andrew-157 <subotinandrey5@gmail.com>
2026-01-01 13:27:33 +0100

    showip - good idea for one of the functionalities of the network tool

--- main.c
+++ main.c
@@ -35,6 +35,16 @@ int main() {
 		}
 
 		struct interface curr;
+		int interface_already_in_array = 0; 
+
+		for (int i = 0; i < count; i++) {
+			if (strcmp(interfaces[i].name, ifa->ifa_name) == 0) {
+				curr = interfaces[i];
+				interface_already_in_array = 1;
+			    break;
+			}
+		}
+
 		curr.name = ifa->ifa_name;
 		s = getnameinfo(
 			ifa->ifa_addr,
@@ -45,7 +55,7 @@ int main() {
 			NI_MAXHOST,
 			NULL,
 			0,
-			NI_NUMERICHOST		
+			NI_NUMERICHOST	
 		);
 
 		if (family == AF_INET) {
@@ -59,18 +69,28 @@ int main() {
 			exit(errno);
 		}
 
-		interfaces = (struct interface *)realloc(interfaces, (count + 1) * sizeof(struct interface));
-		if (interfaces == NULL) {
-			fprintf(stderr, "Failed to allocate memory\n");
-			exit(errno);	
+		if (!interface_already_in_array) {
+			interfaces = (struct interface *)realloc(interfaces, (count + 1) * sizeof(struct interface));
+			if (interfaces == NULL) {
+				fprintf(stderr, "Failed to allocate memory\n");
+				exit(errno);	
+			}
+
+			interfaces[count++] = curr;
 		}
 
-		interfaces[count++] = curr;
+		printf("DEBUG: %s %s %s\n", curr.name, curr.ipv4_addr, curr.ipv6_addr);
 
 	}
 
 	for (int i = 0; i < count; i++) {
-		printf("%s\n", interfaces[i].name);
+		printf("%d. %s\n", (i + 1), interfaces[i].name);
+		if (interfaces[i].ipv4_addr != NULL) {
+			printf("\tinet4: %s\n", interfaces[i].ipv4_addr);
+		}
+		if (interfaces[i].ipv6_addr != NULL) {
+			printf("\tinet6: %s\n", interfaces[i].ipv6_addr);
+		}
 	}
 
 	free(interfaces);
